b2a2fd58d3cef4082c0be49ecb1b4f4f928152c6
Shashank <shashankmadhav10@gmail.com>
2024-04-08 22:34:30 +0530

    vid 140 - 153

--- src/main/java/dev/shashank/rest/webservices/restfulwebservices/helloworld/HelloWorldControlelr.java
+++ src/main/java/dev/shashank/rest/webservices/restfulwebservices/helloworld/HelloWorldControlelr.java
@@ -1,14 +1,24 @@
 package dev.shashank.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldControlelr {
 
-	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+
+	@GetMapping(path = "/hello-world/path-variable-example/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world, %s", name));
 	}
 }
---
+++ src/main/java/dev/shashank/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,53 @@
+package dev.shashank.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 0;
+
+//	The code is enclosed within a static block. 
+//	This means it executes only once, immediately when 
+//	the class is loaded into memory. It's often used for 
+//	initialization tasks that need to happen before the class 
+//	is used.
+	static {
+		users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
+	}
+
+	public List<User> findAll() {
+		return users;
+	}
+
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+//		return users.stream().filter(predicate).findFirst().get();
+		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+//	Predicate<? super User> predicate = user -> user.getId().equals(id);
+//	Predicate<? super User> predicate, this part creates a Predicate object, 
+//	a functional interface used for filtering elements.
+//	It is assigned a lambda function that checks if a given User object has an ID 
+//	that matches the provided id.
+
+//	? super User: This part defines the type of argument the lambda 
+//	function can accept.
+//
+//	The ? signifies a wildcard, indicating some type.
+//	super User specifies that the type can be either User itself or 
+//	any superclass of User.
+
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+}
